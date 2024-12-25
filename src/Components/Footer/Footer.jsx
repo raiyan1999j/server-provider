813c71bf6af7d421652e9d410626198b6b0aef09
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-12-25 12:37:34 +0600

    responsive footer ui

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,9 +2,9 @@
 export default function Footer(){
     return(
         <>
-            <section className="w-[1050px] mx-auto mt-[76px] py-4 border border-[#1D2239] border-r-0 border-b-0 border-l-0 hidden">
-                <div className="w-full mx-auto flex flex-row justify-between">
-                    <div>
+            <section className="w-[1050px] mx-auto mt-[76px] py-4 border border-[#1D2239] border-r-0 border-b-0 border-l-0 mobileS:w-[320px] mobileS:px-4 mobileM:w-[375px] mobileM:px-4 mobileL:w-[425px] mobileL:px-4 tablet:w-[768px] tablet:px-4">
+                <div className="w-full mx-auto flex flex-row justify-between mobileS:flex-col mobileS:items-center mobileS:gap-y-4 mobileM:flex-col mobileM:items-center mobileM:gap-y-4 mobileL:flex-col mobileL:items-center mobileL:gap-y-4">
+                    <div className="mobileS:text-center mobile:text-center mobileL:text-center">
                         <h5 className="font-normal font-dmSans text-sm leading-4 text-[#919DB9]">
                         Copyright © 2024 | <span className="text-[#FFFF]">xCloud Hosting</span> LLC. All rights reserved.
                         </h5>
